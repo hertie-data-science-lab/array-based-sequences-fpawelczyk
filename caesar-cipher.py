56764741b991fb99a8050b8081edf045cf29b36e
fpawelczyk <f.pawelczyk@yahoo.de>
2023-03-12 13:21:22 +0100

    update

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,14 +1,18 @@
-# Author: Fabian Pawelczyk
-# 
+"""
+Date: Feb 19th 2023
+@author: Juan Pablo Brasdefer (juanbrasdefer) Fabian Pawelczyk (fpawelczyk)
+"""
+
 
 class CaesarCipher:
     def __init__(self, shift):
+        # Initialize the class with a given shift value
         self.shift = shift
 
     def encrypt(self, message):
         # Convert message to a list of characters
         chars = list(message)
-        # Loop over each character
+        # Loop over each character in the list
         for i in range(len(chars)):
             # Check if the character is a letter
             if chars[i].isalpha():
@@ -25,12 +29,13 @@ class CaesarCipher:
                     chars[i] = chars[i].lower()
         # Join the list of characters back into a string
         encrypted_message = ''.join(chars)
+        # Return the encrypted message
         return encrypted_message
 
     def decrypt(self, message):
         # Convert message to a list of characters
         chars = list(message)
-        # Loop over each character
+        # Loop over each character in the list
         for i in range(len(chars)):
             # Check if the character is a letter
             if chars[i].isalpha():
@@ -47,7 +52,42 @@ class CaesarCipher:
                     chars[i] = chars[i].lower()
         # Join the list of characters back into a string
         decrypted_message = ''.join(chars)
+        # Return the decrypted message
         return decrypted_message
+
+    def set_shift(self, new_shift):
+        # Set a new shift value
+        self.shift = new_shift
+
+    def get_shift(self):
+        # Get the current shift value
+        return self.shift
+
+
+# Create an instance of the CaesarCipher class with a shift value of 3
+cipher = CaesarCipher(3)
+
+# Encrypt a message using the created cipher
+message = "THE EAGLE IS IN PLAY; MEET AT JOE'S"
+coded = cipher.encrypt(message)
+print("Secret: ", coded)
+
+# Decrypt the encrypted message using the same cipher
+answer = cipher.decrypt(coded)
+print("Message: ", answer)
+
+# Change the shift value to 5
+cipher.set_shift(5)
+
+# Encrypt a new message using the updated cipher
+new_message = "HELLO, WORLD!"
+new_coded = cipher.encrypt(new_message)
+print("Secret: ", new_coded)
+
+# Get the current shift value of the cipher
+current_shift = cipher.get_shift()
+print("Current shift value: ", current_shift)
+
 
 
 cipher = CaesarCipher(3)
